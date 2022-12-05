7980afa63a6994503eb47547184c562b7c5bf2c5
Rajesh Kumar Thankaraj <rajeshkumarthankaraj@Rajeshs-MacBook-Pro.local>
2022-12-05 22:47:30 +0530

    First Upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+import "./App.css";
+
+import RemoveCircleOutlineIcon from "@mui/icons-material/RemoveCircleOutline";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import CachedOutlinedIcon from "@mui/icons-material/CachedOutlined";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  const decrementHandler = () => {
+    setCounter(counter - 1);
+  };
+
+  const incrementHandler = () => {
+    setCounter(counter + 1);
+  };
+
+  const resetHandler = () => {
+    setCounter(0);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container flex-column ">
+      <h1>React Counter</h1>
+      <div className="container-box">
+        <div className="d-flex align-items-center justify-content-center m-5">
+          <h1>{counter}</h1>
+        </div>
+        <div className="d-flex justify-content-around">
+          <RemoveCircleOutlineIcon onClick={decrementHandler} />
+          <CachedOutlinedIcon onClick={resetHandler} />
+          <AddCircleOutlineIcon onClick={incrementHandler} />
+        </div>
+      </div>
     </div>
   );
 }
